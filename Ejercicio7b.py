01fc07260ad2544c958698b64c22e4bc76188d34
martinalejandroguzman82 <88861297+martinalejandroguzman82@users.noreply.github.com>
2021-11-18 18:32:49 -0300

    Delete Ejercicio7b.py

--- Ejercicio7b.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-
-print(" ")
-nombreEmpresa = str(input("Ingrese el nombre de la empresa: "))
-letras = Counter(nombreEmpresa)
-for letter , count in letras.most_common(3):
-    print('{}: {:>7}'.format(letter, count))
